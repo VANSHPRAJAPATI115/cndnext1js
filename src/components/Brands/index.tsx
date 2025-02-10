533005063bbb503664228ade2bfd7089c9abf9b8
Vansh Prajapati <vanshprajapati115@gmail.com>
2025-02-10 21:39:13 +0530

    update company name

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -4,8 +4,9 @@ import brandsData from "./brandsData";
 
 const Brands = () => {
   return (
+    // This section contains the brands component
     <section className="pt-16">
-      <div className="container">
+      {/* <div className="container">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div className="flex flex-wrap items-center justify-center rounded-sm bg-gray-light px-8 py-8 dark:bg-gray-dark sm:px-10 md:px-[50px] md:py-[40px] xl:p-[50px] 2xl:px-[70px] 2xl:py-[60px]">
@@ -15,7 +16,7 @@ const Brands = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
